801da12a4ca26714f6a191a89826b0fe917a6ed2
Alex Craven <w.alex.craven@gmail.com>
2016-08-28 16:58:17 -0500

    edited pipe flow to flow through transform in limit-ten.js

--- word-search.js
+++ word-search.js
@@ -1,16 +1,28 @@
 #!/usr/bin/env node
 
-const {createReadStream} = require('fs')
-const {Writable} = require('stream')
+const { createReadStream } = require('fs')
+const { transformer } = require('./limit-ten.js')
 const readStream = createReadStream('/usr/share/dict/words')
-const transformer = require('./limit-ten.js')
+const { Writable } = require('stream')
 const writeStream = Writable()
 
-readStream//.pipe(transformer)
-readStream.pipe(writeStream)
+const [ , , searchTerm ] = process.argv;
+
+readStream.pipe(transformer)
+          .pipe(writeStream)
 
 
 writeStream._write = (buffer, encoding, cb) => {
   console.log('_write', buffer.toString())
   cb()
 }
+
+
+// var es = require('event-stream')
+// module.exports = function() {
+//   process.stdin                      //connect streams together with `pipe`
+//   .pipe(es.split())                  //split stream to break on newlines
+//   .pipe(es.map(function (data, cb) { //turn this async function into a stream
+//
+//   }))
+// }
