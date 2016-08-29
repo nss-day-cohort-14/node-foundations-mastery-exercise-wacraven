7a686406d6679cdb76f6c50ef50d40e80c4bdc4e
Alex Craven <w.alex.craven@gmail.com>
2016-08-29 11:29:25 -0500

    Search complete, missing 10 response limiter

--- limit-ten.js
+++ limit-ten.js
@@ -2,6 +2,7 @@ const {Transform} = require('stream');
 const transformer = Transform()
 
 transformer._transform = (buffer, encoding, cb) => {
+  // console.log('transform:', buffer.toString());
   cb(null, buffer)
 }
 
--- word-search.js
+++ word-search.js
@@ -5,10 +5,25 @@ const { transformer } = require('./limit-ten.js')
 const readStream = createReadStream('/usr/share/dict/words')
 const { Writable } = require('stream')
 const writeStream = Writable()
-
+const es = require('event-stream')
 const [ , , searchTerm ] = process.argv;
 
-readStream.pipe(transformer)
+readStream.pipe(es.split())                  //split stream to break on newlines
+          .pipe(es.map(function (data, cb) { //turn this async function into a stream
+            if (searchTerm === undefined) {
+              console.log('Usage: word-search.js [searchterm]');
+              process.exit();
+            } else {
+              let testerData = data.toString()
+              if (testerData.toUpperCase().startsWith(searchTerm.toUpperCase())) {
+                console.log(testerData);
+                return testerData
+              } else {
+                cb()
+              }
+            }
+          }))
+          .pipe(transformer)
           .pipe(writeStream)
 
 
@@ -16,13 +31,3 @@ writeStream._write = (buffer, encoding, cb) => {
   console.log('_write', buffer.toString())
   cb()
 }
-
-
-// var es = require('event-stream')
-// module.exports = function() {
-//   process.stdin                      //connect streams together with `pipe`
-//   .pipe(es.split())                  //split stream to break on newlines
-//   .pipe(es.map(function (data, cb) { //turn this async function into a stream
-//
-//   }))
-// }
